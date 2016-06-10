818dce53aa747a0a6fe1953f9e1ca4be6dd218a6
Heber Sandoval <he3ers@gmail.com>
2016-06-10 19:06:44 -0400

    Finish user edit, update, index and destroy actions.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
+
+  def index
+    @users = User.all
+  end
+
   def new
     @user = User.new
   end
@@ -19,9 +27,48 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def edit
+  end
+
+  def update
+    # @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Profile has been updated."
+      redirect_to @user
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted."
+    redirect_to users_path
+  end
+
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  # Before filters
+
+  # Confirms a logged-in user, can't just go to user/1/edit and edit someone elses profile.
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "Please log in."
+      redirect_to login_path
+    end
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_path) unless current_user?(@user) # @user == current_user
+  end
+
+  # confims an admin user, setting access control to the destroy action
+  def admin_user
+    redirect_to(root_path) unless current_user.admin?
   end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,4 +1,6 @@
 module SessionsHelper
+
+  #logs in the given user
   def log_in(user)
     # rails method: make temporary cookies using the session method that are automatically ecncrypted
     # and expire automatically on browser close
@@ -10,6 +12,11 @@ module SessionsHelper
     # storing in a instance variable, which hits the db the 1st time but
     # returns the instance variable immediately on subsequents invocations
     @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  # returns true if the given user is the current user, also replaces the current_user method
+  def current_user?(user)
+    user == current_user
   end
 
   # returns true if the user is logged in, failure otherwise
